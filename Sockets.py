9269cfbea37a9a0d1fa640b4f5d86091ed320890
Lauradelmar <lauradelmarjr@gmail.com>
2024-12-13 16:39:28 +0000

    Comentarios añadidos a Sockets.py

--- Sockets.py
+++ Sockets.py
@@ -1,15 +1,15 @@
 import socket
 
-import socket
+ip_address = socket.gethostbyname('google.com') # Obtener la dirección IP de un dominio
+ip_name = socket.gethostbyaddr('142.250.201.67') # Obtener el nombre del host a partir de una dirección IP
 
-# Obtener la dirección IP de un dominio
-ip_address = socket.gethostbyname('google.com')
-ip_name = socket.gethostbyaddr('142.250.201.67')
+# Devuelve una tupla que incluye:
+# (nombre canónico del host, lista de alias, lista de direcciones IP asociadas).
 ex_address = socket.gethostbyname_ex('google.com')
-fqdn = socket.getfqdn('google.com')
-port_number = socket.getservbyname('http')
-port_number2 = socket.getservbyname('http', 'tcp')
-nombre_puerto = socket.getservbyport(25)
+fqdn = socket.getfqdn('google.com') # Obtener el nombre de dominio completamente calificado (FQDN)
+port_number = socket.getservbyname('http') # Obtener el número de puerto por el nombre del servicio
+port_number2 = socket.getservbyname('http', 'tcp') # Obtener el número de puerto para HTTP utilizando TCP
+nombre_puerto = socket.getservbyport(25) # Obtener el nombre del servicio a partir de un número de puerto
 
 print("gethostbyname: ",ip_address)
 print("gethostbyaddr:", ip_name)
@@ -18,15 +18,6 @@ print("fqdn: ",fqdn)
 print("port number: ",port_number)
 print("port number 2: ",port_number2)
 print("port name: ", nombre_puerto)
-
-
-
-
-    
-
-
-
-
 
 
 pass
